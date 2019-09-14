305b388cb8dd010844e6b4b0cc98f7543a8dde12
Yuuki <minhphuongsony@gmail.com>
2019-09-14 19:05:57 +1000

    Remove dist folder

--- dist/index.js
+++ dist/index.js
@@ -4,3 +4,22 @@ var httpCode_1 = require("./data/httpCode");
 exports.HttpCode = httpCode_1.HttpCode;
 var generateOutput_1 = require("./generateOutput");
 exports.HttpResponse = generateOutput_1.HttpResponse;
+var generateOutput_2 = require("./generateOutput");
+var a = new generateOutput_2.HttpResponse();
+var test = a
+    .addData({
+    data: 1,
+    httpCode: 200
+})
+    .toOutput();
+var test2 = a
+    .addError({
+    clientMessage: '1',
+    httpCode: 500,
+    technicalError: {
+        error: 1
+    }
+})
+    .toOutput();
+console.log(test);
+console.log(test2);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
 export { HttpCode } from './data/httpCode';
 
 export { HttpResponse } from './generateOutput';
+
+import { HttpResponse } from './generateOutput';
+
+const a = new HttpResponse();
+
+const test = a
+  .addData({
+    data: 1,
+    httpCode: 200
+  })
+  .toOutput();
+
+const test2 = a
+  .addError({
+    clientMessage: '1',
+    httpCode: 500,
+    technicalError: {
+      error: 1
+    }
+  })
+  .toOutput();
+
+console.log(test);
+console.log(test2);
